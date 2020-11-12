740a7a214e695bc43632a0b84ef9bfd284d23b4d
bboyfat <bboyfat12@gmail.com>
2020-11-12 17:44:28 +0200

    Initial Commit

---
+++ GenTechTests/GenTechTests.swift
@@ -0,0 +1,33 @@
+//
+//  GenTechTests.swift
+//  GenTechTests
+//
+//  Created by Andrey Petrovskiy on 12.11.2020.
+//
+
+import XCTest
+@testable import GenTech
+
+class GenTechTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
